b17519155628930d39ad5473b6fc4a37322f8b38
Prabhu Moorthy <prabhu.hotpepper@gmail.com>
2015-10-15 12:04:05 +0530

    merged NCDC examples

---
+++ MapReduceTryouts/src/test/java/org/ncdc/MaxTempTest/FileReader.java
@@ -0,0 +1,12 @@
+package org.ncdc.MaxTempTest;
+
+import java.io.FileNotFoundException;
+
+public class FileReader {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		
+
+	}
+
+}
