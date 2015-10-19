eabacc4402c75c2b7a6fc043b86534535630915b
Prabhu Moorthy <prabhu.hotpepper@gmail.com>
2015-10-19 20:58:12 +0530

    Added test class for NCDC

--- MapReduceTryouts/src/main/java/org/ncdc/MaxTemp/ClimateMapper.java
+++ MapReduceTryouts/src/main/java/org/ncdc/MaxTemp/ClimateMapper.java
@@ -17,7 +17,7 @@ public class ClimateMapper extends org.apache.hadoop.mapreduce.Mapper<LongWritab
 		throws IOException,InterruptedException {
 		
 		String line = value.toString();
-		String year = line.substring(7, 11);
+		String year = line.substring(15, 19);
 		int airTemp;
 		if(line.charAt(87) == '+'){
 			airTemp = Integer.parseInt(line.substring(88, 92));
