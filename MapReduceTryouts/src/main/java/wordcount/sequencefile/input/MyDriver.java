2217fcc3bdf35ff27e363e100f57e3c3bdf9fac8
Prabhu Moorthy <prabhu.hotpepper@gmail.com>
2015-10-22 19:35:35 +0530

    Completed Sequence output

--- MapReduceTryouts/src/main/java/wordcount/sequencefile/input/MyDriver.java
+++ MapReduceTryouts/src/main/java/wordcount/sequencefile/input/MyDriver.java
@@ -3,8 +3,10 @@ package wordcount.sequencefile.input;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.lib.IdentityMapper;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFilter;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -25,11 +27,11 @@ public class MyDriver extends Configured implements Tool {
 		Path outputPath =  new Path(args[1]);
 		outputPath.getFileSystem(getConf()).delete(outputPath, true);
 		
-		job.setMapperClass(MyMapper.class);
-		job.setReducerClass(MyReducer.class);
+		//job.setMapperClass(IdentityMapper.class);
+		//job.setReducerClass(Reducer.class);
 		
-		job.setMapOutputKeyClass(NullWritable.class);
-		job.setMapOutputValueClass(Text.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
 		
 		job.setInputFormatClass(SequenceFileInputFilter.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
